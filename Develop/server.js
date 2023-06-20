c41b4085acb8c00e2c33bb297b218cca6b886af1
Potatonaut <stam296@gmail.com>
2023-06-20 01:16:46 -0500

    Deleted routes folder since starter code already contained a index.js file. Update server.js to use express router and corrected some of the paths so that the /notes page works. /notes was not functioning in the previous commit

--- Develop/server.js
+++ Develop/server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const path = require('path');
+const router = express.Router();
 const notesData = require('./db/db.json');
 
-const api = require('./routes/index');
+// const api = require('./routes/index');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -10,16 +11,18 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static('public'));
+app.use(express.static(path.join('public')));
 
-app.use('/api', api);
+// app.use('/api', api);
+
+var publicPath = path.join(__dirname, 'public');
 
 app.get('/', (req, res) => 
     res.sendFile(path.join(_dirname, './public/index.html'))
 );
 
 app.get('/notes', (req, res) => 
-    res.sendFile(path.join(_dirname, './public/notes.html'))
+    res.sendFile(publicPath +  '/notes.html')
 );
 
 app.listen(PORT, () =>
